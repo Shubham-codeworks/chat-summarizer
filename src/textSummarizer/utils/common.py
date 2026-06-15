fffe2a6c08a1797a06d4e8f28c1d299ca920ff41
Shubham Choudhary <shubham.workplaces@gmail.com>
2026-06-15 07:26:52 +0530

    feat: implement data ingestion
    - Added DataIngestion components including download and extraction logic

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -37,7 +37,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
 
 
 @ensure_annotations
-def create_directories(path_to_directories, list, verbose=True):
+def create_directories(path_to_directories, verbose=True):
     """Create list of directories
 
     Args:
