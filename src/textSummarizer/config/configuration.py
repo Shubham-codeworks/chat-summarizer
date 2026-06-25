85dee2f96db6e3e908faf68ea8fa5c6340fde0bf
Shubham Choudhary <shubham.workplaces@gmail.com>
2026-06-25 20:04:32 +0530

    feat: Completed model training stage

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -3,7 +3,8 @@ from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
 from textSummarizer.entity import (DataIngestionConfig, 
                                    DataValidationConfig, 
-                                   DataTransformationConfig)
+                                   DataTransformationConfig,
+                                   ModelTrainerConfig)
 
 
 class ConfigurationManager:
@@ -58,3 +59,27 @@ class ConfigurationManager:
         )
 
         return data_transformation_config
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.TrainingArguments
+        root_dir = Path(config.root_dir)
+        
+        create_directories(path_to_directories=[root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=Path(config.root_dir),
+            data_path=Path(config.data_path),
+            model_ckpt=config.model_ckpt,
+            num_train_epochs=params.num_train_epochs,
+            warmup_steps=params.warmup_steps,
+            per_device_train_batch_size=params.per_device_train_batch_size,
+            weight_decay=params.weight_decay,
+            logging_steps=params.logging_steps,
+            eval_strategy=params.eval_strategy,
+            eval_steps=params.eval_steps,
+            save_steps=int(float(params.save_steps)),
+            gradient_accumulation_steps=params.gradient_accumulation_steps
+        )
+
+        return model_trainer_config
