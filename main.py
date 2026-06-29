8e42ecef85ad15bce3322904fd08184b220f8bb4
Shubham Choudhary <shubham.workplaces@gmail.com>
2026-06-29 20:19:32 +0530

    feat(eval): evaluate fine-tuned Pegasus model on SAMSum dataset
    - Completed model evaluation stage utilizing T4 GPU acceleration
    
    - Evaluated 818 test instances with a batch size of 8
    
    - Exported final performance scores to artifacts/model_evaluation/metrics.csv

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainin
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarizer.logging import logger
 import os
 os.environ.pop("SSL_CERT_FILE", None)
@@ -46,3 +47,13 @@ os.environ.pop("SSL_CERT_FILE", None)
 # except Exception as e:
 #     logger.exception(e)
 #     raise e
+
+STAGE_NAME = "Model Evaluation Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} Started <<<<<<")
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
+    logger.info(f">>>>>> {STAGE_NAME} Completed Successfully! <<<<<<\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
